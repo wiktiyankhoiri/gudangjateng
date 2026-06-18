7adc114f8764c061773be87cacab4f962d73273a
wiktiyankhoiri <wiktiyankhoiri@gmail.com>
2026-06-18 16:55:38 +0700

    Rubah placeholder .com menjadi web.id

--- resources/views/pengaturan/management-user/edit.blade.php
+++ resources/views/pengaturan/management-user/edit.blade.php
@@ -31,7 +31,7 @@
                     <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">Email</label>
                     <input type="email" name="email" value="{{ old('email', $data->email) }}"
                         class="h-11 w-full rounded-lg border dark:bg-dark-900 border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800">
-                    <p class="mt-1 text-xs text-gray-500 dark:text-gray-400">Kosongkan untuk auto (username@gudangjateng.com)</p>
+                    <p class="mt-1 text-xs text-gray-500 dark:text-gray-400">Kosongkan untuk auto (username@gudangjateng.web.id)</p>
                 </div>
                 <div>
                     <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">Password Baru</label>
